233ef82a5ea8e25c1ed8d64a78f3aa6ef6e3735e
Nic Newdigate <nicnewdigate@gmail.com>
2019-02-02 17:27:02 +0000

    Create TFTSdDirectoryBrowser.h

---
+++ TFTSdDirectoryBrowser.h
@@ -0,0 +1,107 @@
+//
+// Created by Nicholas Newdigate on 02/02/2019.
+//
+
+#ifndef ARDUINO_MIDI_WRITER_TFTSDDIRECTORYBROWSER_H
+#define ARDUINO_MIDI_WRITER_TFTSDDIRECTORYBROWSER_H
+
+#ifndef USE_SD_H
+#define USE_SD_H 0
+#endif
+
+#if ARDUINO >= 100
+  #include <Adafruit_GFX.h>    // Core graphics library
+
+  #if USE_SD_H
+    #include <SD.h>
+  #else  // USE_SD_H
+    #include "SdFs.h"
+  #endif  // USE_SD_H
+  
+#else
+  #include "Adafruit/Adafruit_GFX.h"    // x86 emulated core graphics library
+  #include "SD/SD.h"                    // x86 emulated sd card 
+#endif
+
+class TFTSdDirectoryBrowser {
+public:
+    inline TFTSdDirectoryBrowser(
+                Adafruit_GFX *tft,
+                SdFat *sd,
+                int nMax,
+
+                uint16_t cDirectoryForeground,
+                uint16_t cDirectoryBackground,
+                uint16_t cDirectorySelectedForeground,
+                uint16_t cDirectorySelectedBackground,
+                uint16_t cFileForeground,
+                uint16_t cFileBackground,
+                uint16_t cFileSelectedForeground,
+                uint16_t cFileSelectedBackground
+
+                ) :
+
+            _tft(tft),
+            _sd(sd),
+            _nMaxFilesPerPage(nMax),
+             _cDirectoryForeground(cDirectoryForeground),
+             _cDirectoryBackground(cDirectoryBackground),
+             _cDirectorySelectedForeground(cDirectorySelectedForeground),
+             _cDirectorySelectedBackground(cDirectorySelectedBackground),
+             _cFileForeground(cFileForeground),
+             _cFileBackground(cFileBackground),
+             _cFileSelectedForeground(cFileSelectedForeground),
+             _cFileSelectedBackground(cFileSelectedBackground)
+    {
+        filenames = new char*[nMax];
+        dirIndex = new uint16_t[nMax];
+    }
+
+    inline ~TFTSdDirectoryBrowser(){
+        for (int i=0; i<_nMaxFilesPerPage;i++)
+            if (filenames[i])
+                delete [] filenames[i];
+
+        delete [] filenames;
+    }
+
+    void initialize();
+    void reload();
+    void setSelectFileIndex(int index);
+    void update();
+    unsigned long getTotalFileCount();
+private:
+    long _currentPage   = 0;
+    long _selectedFileIndex     = -1;
+    long _totalFiles    = 0;
+    uint16_t _nMaxFilesPerPage;
+    uint16_t _numFilesOnPage;
+    bool _enabled       = false;
+
+    uint16_t _cDirectoryForeground;
+    uint16_t _cDirectoryBackground;
+    uint16_t _cDirectorySelectedForeground;
+    uint16_t _cDirectorySelectedBackground;
+    uint16_t _cFileForeground;
+    uint16_t _cFileBackground;
+    uint16_t _cFileSelectedForeground;
+    uint16_t _cFileSelectedBackground;
+
+    File file;
+    File dirFile;
+    SdFat *_sd;
+    Adafruit_GFX *_tft;
+    char **filenames;
+    uint16_t *dirIndex;
+
+    bool isHidden(const File &file);
+    bool openNext(File &file, File &dirFile);
+    void rewind(File &file);
+    void getFileName(File &file, char *buf);
+    void openFileIndex(File &file, File &dir, int index);
+    bool isSubDir(File &file);
+    void toggleSelection(int i, bool selected);
+};
+
+
+#endif //ARDUINO_MIDI_WRITER_TFTSDDIRECTORYBROWSER_H
